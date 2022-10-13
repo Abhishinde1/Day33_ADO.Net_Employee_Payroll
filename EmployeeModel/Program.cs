10c2c15fd8ea4f926ca6420b761d643c3186de91
AbhiShinde1 <abhishinde572@gmail.com>
2022-10-13 22:28:52 +0530

    UC1 created

---
+++ EmployeeModel/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeModel
+{
+    public class Program
+    {
+      public  static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Welcome to Employee payroll");
+            EmployeeModel model = new EmployeeModel();
+           
+            model.EmployeeName = "Bruce";
+            model.PhoneNumber = 1234567890;
+            model.Address = "Banglore";
+            model.Department = "HR";
+            model.Gender = 'M';
+            model.BasicPay = 22000;
+            model.Deduction = 1500;
+            model.TaxablePay = 200;
+            model.Tax = 300;
+            model.NetPay = 2500;
+            model.City = "Benglore";
+            model.Country = "India";
+
+            EmployeeRepo repo = new EmployeeRepo();
+            //repo.AddEmployee(model);
+           repo.GetAllEmployee();
+        }
+    }
+}
